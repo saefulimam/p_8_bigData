ab8addfec38368cda66e075f411e825918e42266
saefulimam <56109440+saefulimam@users.noreply.github.com>
2020-05-01 15:37:56 +0700

    Add files via upload

---
+++ p08_bigdata/src/mapreduce/koneksi.java
@@ -0,0 +1,19 @@
+package mapreduce;
+
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoDatabase;
+
+public class koneksi {
+
+    public static MongoDatabase sambungDB() {
+        try {
+            MongoClient client = MongoClients.create();
+            MongoDatabase database = client.getDatabase("bigdata");
+            return database;
+        } catch (Exception e) {
+            System.out.println("Koneksi Gagal: " + e.getMessage());
+        }
+        return null;
+    }
+}
